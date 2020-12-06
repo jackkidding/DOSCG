b29204d93e926d64c6172024f234ccc71f48fe9d
Pongsathorn Chutachuen <pongsathorn@fungjai.com>
2020-12-06 23:54:13 +0700

    feat(google map): add google map and direction feature

--- web-front-end/src/components/App/index.js
+++ web-front-end/src/components/App/index.js
@@ -1,25 +1,71 @@
+/*global google*/
+
 import logo from './logo.svg';
 import './style.css';
+import { GoogleMap } from '@googlemaps/map-loader'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapOptions = {
+  zoom: 1
+}
+
+const apiOptions = {
+  version: 'weekly',
+}
+
+const mapLoaderOptions = {
+  apiKey: "AIzaSyC-xEY8-RgDdteff9etRy-SL4ehz5H6jbQ",
+  divId: 'map',
+  append: false,
+  mapOptions: mapOptions,
+  apiOptions: apiOptions
+};
+
+const mapLoader = new GoogleMap()
+
+const initMap = () => {
+  mapLoader.initMap(mapLoaderOptions)
+  .then(googleMap => {
+
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
+    directionsRenderer.setMap(googleMap)
+    directionsRenderer.setPanel(document.getElementById('directionsPanel'))
+
+    calculateAndDisplayRoute(directionsService, directionsRenderer)
+
+  })
+}
+
+const calculateAndDisplayRoute = (directionsService, directionsRenderer) => {
+
+  const request = {
+      origin: { placeId: 'ChIJW-qHMHOc4jARHdzY5oFpy-c' },
+      destination: { placeId: 'ChIJ4VX0ws-e4jARBGaQ2IACrcQ' },
+      travelMode: google.maps.TravelMode.DRIVING,
+  }
+  
+  directionsService.route(request, (result, status) => {
+      if (status === "OK") {
+        directionsRenderer.setDirections(result);
+      }
+  })
+}
+
+
+class App extends React.Component {
+  
+    render() {
+
+      initMap()
+
+      return (
+        <div>
+          <div id="map" style={{width: "70%", height: 1100, float:"right"}}></div>
+          <div id="directionsPanel" style={{width: "30%", height: 1100, float:"left"}}></div>
+        </div>
+      );
+    }
 }
 
 export default App;
