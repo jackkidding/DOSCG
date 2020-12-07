14890c109fedc6bd750c2512fad39aa5e1a670eb
Pongsathorn Chutachuen <pongsathorn@fungjai.com>
2020-12-07 23:35:00 +0700

    refactor(routes): change route for map component

--- web-front-end/src/routes.js
+++ web-front-end/src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Router, Route } from 'react-router';
 
-import App from './components/App';
+import Map from './components/Map';
 
 const Routes = (props) => (
   <Router {...props}>
-    <Route path="/" component={App} />
+    <Route path="/DOSCG/map" component={Map} />
   </Router>
 );
 
